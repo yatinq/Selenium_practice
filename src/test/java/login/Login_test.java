b82613b9646be6c2b22ba391ffaff203b01c03a8
bijnis <bijnis@ip-192-168-1-87.ap-south-1.compute.internal>
2025-07-09 16:53:40 +0530

    verify login text

--- src/test/java/login/Login_test.java
+++ src/test/java/login/Login_test.java
@@ -1,6 +1,9 @@
 package login;
 
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
@@ -8,8 +11,15 @@ import base.BaseTest;
 public class Login_test extends BaseTest{
 	
 	@Test()
-	public void login() {
+	public void verify_loginText() {
 		
+		String expectedtext = prop.getProperty("success_login_heading");
+		
+		String actualtext = driver.findElement(By.tagName(loc.getProperty("success_login_heading"))).getText();
+		
+		Assert.assertEquals(actualtext, expectedtext);
+		
+		log.info("Actual text is same as expected text : "+actualtext);
 	}
 	
 	@Test()
